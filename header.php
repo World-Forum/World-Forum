41e50019d982d70df633e8e355f9d33b5c70d8df
krack3 <mike@krackeonline.com>
2016-11-28 10:46:55 -0600

    Header Update
    Updated the menus in the header

--- header.php
+++ header.php
@@ -27,41 +27,39 @@ $main_menu_items_wrap =
 	<div class="wrapper" >
 		<div class="top-float-bar">
 			<div class="search-bar">
-				<?php  if ( is_user_logged_in() ) { ?>
-					<div class="logout"><a class="logout" href="/account-settings/" >Account Settings</a></div>
-					<div class="logout"><a class="logout" href="<?php echo wp_logout_url( home_url() ); ?>">Logout</a></div>
-					<?php } else { ?>
-						<div class="logout"><a class="logout" href="https://connect.worldforumfoundation.org/login" ?>Login</a></div>
-						<?php } ?>
-						<div class="search-bar">
-							<form  role="search" action="<?php echo home_url( '/' ); ?>" method="get">
-								<input type="text" name="s" id="search" placeholder=' Search the <?php echo bloginfo('name'); ?>' value="<?php the_search_query(); ?>" />
-							</form>
-						</div>
-					</div>
-				</div>
-				<?php if ( ( $blog_id == 1 ) && ( is_front_page() ) )  { } else if ( $blog_id == 8 )  { ?>
-					<header class='<?php echo 'site-header ' . $header_css_class; ?>'>
-						<div class='<?php echo 'header-logo ' . $header_logo_css_class ?>'><a href='<?php echo get_site_url(2); ?>' title='<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>' rel='home'><img src='<?php echo esc_url( get_theme_mod( 'themeslug_logo' ) ); ?>' alt='<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>'></a></div>
-						<?php wp_nav_menu( array(
-							'menu' => 'Primary Nav',
-							'container_id' => 'primary-nav-container',
-							'menu_class' => 'primary-nav-menu',
-							'theme_location' => 'primary-nav',
-							'items_wrap' => $main_menu_items_wrap,
-							'depth' => 2) ); ?>
-						</header>
-						<?php }  else { ?>
-							<header class='<?php echo 'site-header ' . $header_css_class; ?>'>
-								<div class='<?php echo 'header-logo ' . $header_logo_css_class ?>'><a href='<?php echo get_site_url(); ?>' title='<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>' rel='home'><img src='<?php echo esc_url( get_theme_mod( 'themeslug_logo' ) ); ?>' alt='<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>'></a></div>
-								<?php wp_nav_menu( array(
-									'menu' => 'Primary Nav',
-									'container_id' => 'primary-nav-container',
-									'menu_class' => 'primary-nav-menu',
-									'theme_location' => 'primary-nav',
-									'items_wrap' => $main_menu_items_wrap,
-									'depth' => 2) ); ?>
-								</header>
-								<?php }  ?>
+				<form  role="search" action="<?php echo home_url( '/' ); ?>" method="get">
+					<input type="text" name="s" id="search" placeholder=' Search the <?php echo bloginfo('name'); ?>' value="<?php the_search_query(); ?>" />
+				</form>
+			</div>
+			<?php wp_nav_menu( array(
+				'menu' => 'Top Nav',
+				'container_id' => 'top-nav-container',
+				'menu_class' => 'top-nav-menu',
+				'theme_location' => 'top-nav',
+				'depth' => 1) ); ?>
+			</div>
+			<?php if ( ( $blog_id == 1 ) && ( is_front_page() ) )  { } else if ( $blog_id == 8 )  { ?>
+				<header class='<?php echo 'site-header ' . $header_css_class; ?>'>
+					<div class='<?php echo 'header-logo ' . $header_logo_css_class ?>'><a href='<?php echo get_site_url(2); ?>' title='<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>' rel='home'><img src='<?php echo esc_url( get_theme_mod( 'themeslug_logo' ) ); ?>' alt='<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>'></a></div>
+					<?php wp_nav_menu( array(
+						'menu' => 'Primary Nav',
+						'container_id' => 'primary-nav-container',
+						'menu_class' => 'primary-nav-menu',
+						'theme_location' => 'primary-nav',
+						'items_wrap' => $main_menu_items_wrap,
+						'depth' => 2) ); ?>
+					</header>
+					<?php }  else { ?>
+						<header class='<?php echo 'site-header ' . $header_css_class; ?>'>
+							<div class='<?php echo 'header-logo ' . $header_logo_css_class ?>'><a href='<?php echo get_site_url(); ?>' title='<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>' rel='home'><img src='<?php echo esc_url( get_theme_mod( 'themeslug_logo' ) ); ?>' alt='<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>'></a></div>
+							<?php wp_nav_menu( array(
+								'menu' => 'Primary Nav',
+								'container_id' => 'primary-nav-container',
+								'menu_class' => 'primary-nav-menu',
+								'theme_location' => 'primary-nav',
+								'items_wrap' => $main_menu_items_wrap,
+								'depth' => 2) ); ?>
+							</header>
+							<?php }  ?>
 
-								<body <?php body_class(); ?>>
+							<body <?php body_class(); ?>>
