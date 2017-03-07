7084e250c6836540e90548793388133cd8d9a487
krack3 <mike@krackeonline.com>
2017-03-07 09:12:52 -0600

    Moved Galler.css to image.scss

--- functions.php
+++ functions.php
@@ -12,8 +12,8 @@ function wff_theme_styles() {
 
 	wp_enqueue_style( 'the_css', get_template_directory_uri() . '/css/main.min.css' );
 	wp_enqueue_style( 'main_css', get_template_directory_uri() . '/style.css' );
-	wp_enqueue_style( 'gallery_css', get_template_directory_uri() . '/css/gallery.min.css' );
-	wp_enqueue_style( 'fonts_css', get_template_directory_uri() . '/css/fonts.min.css' );
+//	wp_enqueue_style( 'gallery_css', get_template_directory_uri() . '/css/gallery.min.css' );
+//	wp_enqueue_style( 'fonts_css', get_template_directory_uri() . '/css/fonts.min.css' );
 	wp_enqueue_style( 'custom_plugin_style_css', get_template_directory_uri() . '/css/custom-plugin-style.min.css' );
 	wp_enqueue_style( 'media_queries_css', get_template_directory_uri() . '/css/media-queries.min.css' );
 	//	wp_enqueue_script( 'java', get_template_directory_uri() . '/js/main.js' );
